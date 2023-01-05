c93a602cd92fe03c6f04e46a2021318e9e1ad41c
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-01-05 23:15:17 +0100

    useState for Array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
 
 function App() {
   return (
     <React.Fragment>
       <div className="container">
-        <Setup />
+        <UseStateArray />
       </div>
     </React.Fragment>
   );
